51c67ebeff6856baf0d69ae8a80573d6233661b1
Đức <64053044+steveho29@users.noreply.github.com>
2021-07-19 21:19:25 +0700

    Update sort.c

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -31,13 +31,14 @@ int main()
 
     // Bubble sort
     for (int i = 0; i<n-1; i++)
-        for (int j = 0; j<n-i-1; j++)
+        for (int j = 0; j<n-i-1; j++){
             if (A[j] > A[j+1])
             {
                 int tmp = A[j];
                 A[j] = A[j+1];
                 A[j+1] = tmp;
             }
+        }
     
 
 
