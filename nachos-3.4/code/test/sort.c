7d5d2d2a0eea919173d70bd044b6f1ed6fc68d4c
Đức <64053044+steveho29@users.noreply.github.com>
2021-07-19 21:05:00 +0700

    Update sort.c

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -23,12 +23,14 @@ int main()
         return 0;
     }
 
-    for (int i = 0; i<n; i++)
-        A[i] = ReadInt();
+    for (int i = 0; i<n; i++){
+        int num = ReadInt();
+        A[i] = num;
+    }
     
 
     // Bubble sort
-    for (int i = 0; i<n-1; i++){
+    for (int i = 0; i<n-1; i++)
         for (int j = 0; j<n-i-1; j++)
             if (A[j] > A[j+1])
             {
@@ -36,7 +38,7 @@ int main()
                 A[j] = A[j+1];
                 A[j+1] = tmp;
             }
-    }
+    
 
 
     // Output sorted Array
